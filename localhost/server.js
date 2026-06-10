d2ed5ad9e5aaeb4014014d920842cb48fa5897f8
yu-mathy14 <yuna14.navi1031@gmail.com>
2026-06-10 15:38:36 +0900

    localhost接続のための環境構築およびフォルダ整理

---
+++ localhost/server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer(function(req, res){
+  const filePath = path.join(__dirname, '..', 'localhost', 'index.html');
+
+  fs.readFile(filePath, 'utf-8', function(err, data) {
+    if (err) {
+      res.writeHead(500,{
+        'Content-Type': 'text/plain'
+      });
+      res.end('Server Error');
+      return;
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    res.end(data);
+  });
+});
+
+server.listen(3000, '0.0.0.0', function(){
+  console.log('Server running on port 3000');
+});
