360e487ca16b86de64d25f121375c4bbab7d381c
yu-mathy14 <yuna14.navi1031@gmail.com>
2026-06-11 16:59:10 +0900

    jsコードの理解のためのコメントアウト

--- localhost/index.js
+++ localhost/index.js
@@ -1,5 +1,5 @@
 console.log("====自動販売機====");
-// 飲み物温度の定数の定義
+// 飲み物温度の表示文言を一元管理するための変数
 /* 配列のtempのvalueに直接"あたたかい"や"つめたい"
    を打ち込むとタイプミスの可能性あり */
 /* 後から温度の表記を変えたい時に楽
@@ -9,7 +9,7 @@ const TEMP = {
   COLD: "つめた〜い"
 };
 // 商品を配列で管理
-const product = [
+const products = [
   {
     name: "水",
     price: 100,
@@ -49,15 +49,23 @@ const product = [
 ];
 
 // 商品一覧表示
-/*  */
+/* HTML要素<section id="product-list"></section>への参照 */
 const productList = document.getElementById("product-list");
+// 商品一覧を画面に表示する(再表示)するための関数
 function renderProducts(){
+  /* .innerHTML -> HTML文字列を要素に変換して中に入れる */
+  /* productList.innerHTML -> productListの中にあるHTMLをすべて削除 */
   productList.innerHTML = "";
-  product.forEach((item, index) => {
+  /* forEach() -> 配列の要素を順番に1つずつ取り出して指定した処理を繰り返す
+     item -> 配列内のオブジェクトの仮名(商品情報を取り出す)
+     index -> インデックス番号(商品番号を取り出す) */
+  products.forEach((item, index) => {
     // 温度判定の定義
     const tempClass =
       item.temp === TEMP.HOT ? "hot" : "cold";
-
+    
+    // 商品カードを作るための<div>要素の作成
+    /* HTML要素への参照 */
     const card = document.createElement("div");
     card.className = "card";
 
@@ -67,6 +75,8 @@ function renderProducts(){
     const disabledAttr = isSoldOut ? "disabled" : "";
     const buttonText = isSoldOut ? "売切" : "購入";
 
+    // 商品カードを作成する -> <div>タグの所に追加
+    /* .innerHTML -> HTML文字列を要素に変換して中に入れる */
     card.innerHTML = `
       <p class="product-name">${item.name}
         <span>${item.price}円</span>
@@ -79,44 +89,58 @@ function renderProducts(){
       </button>
     `;
 
+    // 作った商品カードを商品一覧に追加する
+    /* .appendChild -> 要素の末尾に子要素を追加する */
     productList.appendChild(card);
   });
 }
+// 商品在庫の初期状態を表示
 renderProducts();
 
-// 履歴管理用の共通関数の定義
+// 履歴管理用
+/* HTML要素への参照 */
 const result = document.getElementById("result");
-
-let purchaseLog = [];
-
+/* ログを格納する空配列の用意 */
+const purchaseLog = [];
+/* ログを追加する関数 */
 function addLog(message) {
+  /* .push() -> 配列の最後に追加する */
   purchaseLog.push(message);
-
+  /* .innerHTML -> HTML文字列を要素に変換して中に入れる */
   result.innerHTML = purchaseLog
+    // mapで各ログを配列に変換
+    /* .map() -> 元の配列を加工して新しい配列を作る */
     .map(log => `<p>${log}</p>`)
+    // join("")で1つのHTML文字列にする
     .join("");
 }
 
 
 // 投入金額の管理
+/* 合計投入金額の初期値の定義 */
 let insertedMoney = 0;
-
-const moneyDisplay =
-  document.getElementById("money-display");
+/* HTML要素への参照 */
+const moneyDisplay = document.getElementById("money-display");
 
 // 投入時
 function insertMoney(amount){
+  /* 条件分岐で投入金額の上限を設定 */
   if(insertedMoney + amount > 1000){
     alert("1000円以上は投入できません");
     return;
   }
+
+  /* 合計投入金額に押されたボタンの数字(amount)を足して、
+     その金額を合計投入金額とする */
   insertedMoney += amount;
+  /* moneyDisplayに合計投入金額を表示する */
+  /* .textContent -> HTMLとして解釈せず文字として表示する */
   moneyDisplay.textContent = insertedMoney;
 }
 
 // 購入処理
 function buyProduct(index) {
-  const selectedProduct = product[index];
+  const selectedProduct = products[index];
 
   // 売切れチェック
   /* 処理側の制御(正しさ)
@@ -129,7 +153,7 @@ function buyProduct(index) {
   }
 
 // 金額チェック
-  if(insertedMoney <selectedProduct.price){
+  if(insertedMoney < selectedProduct.price){
     addLog(
       `購入失敗：${selectedProduct.name}の購入には ${
       selectedProduct.price - insertedMoney}円不足しています`
@@ -141,19 +165,21 @@ function buyProduct(index) {
   insertedMoney -= selectedProduct.price;
   selectedProduct.stock--;
 
+  /* .textContent -> HTMLとして解釈せず文字として表示する */
   moneyDisplay.textContent = insertedMoney;
 
   // ログに追加
   addLog(
-  `${selectedProduct.name}を購入しました(残高：${insertedMoney}円)`
+  `${selectedProduct.name}(${selectedProduct.temp})を購入しました(残高：${insertedMoney}円)`
   );
 
+  // 商品購入後、変動した在庫を反映してから商品一覧を再表示
   renderProducts();
 }
 
 // 返金機能
-const refundBtn =
-  document.getElementById("refundBtn");
+/* HTML要素への参照 */
+const refundBtn = document.getElementById("refundBtn");
 
 refundBtn.addEventListener(
   "click",
@@ -171,6 +197,7 @@ refundBtn.addEventListener(
 
     insertedMoney = 0;
 
+    /* .textContent -> HTMLとして解釈せず文字として表示する */
     moneyDisplay.textContent = insertedMoney;
 
     addLog(
